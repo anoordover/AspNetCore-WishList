b7ce9df3dc92856857d25bf52be736a07c24f57a
Arno Noordover <arno@noordover.net>
2021-12-01 22:57:50 +0100

    DB context

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Model;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
